e2bfb7680ee0fa366b2f863f89c433d879903d5c
hengchu <hengchu.zhang@gmail.com>
2016-12-04 22:45:12 -0500

    kvtool: adding -p option for value path

--- src/kvtool/main.cpp
+++ src/kvtool/main.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <fstream>
 #include <kvapi.h>
 #include <unistd.h>
 #include <semaphore.h>
+
+std::string
+readFile(const char *path)
+{
+  // open the file:
+  std::ifstream file(path, std::ios::binary);
+
+  // Stop eating new lines in binary mode!!!
+  file.unsetf(std::ios::skipws);
+
+  // get its size:
+  std::streampos fileSize;
+
+  file.seekg(0, std::ios::end);
+  fileSize = file.tellg();
+  file.seekg(0, std::ios::beg);
+
+  // reserve capacity
+  std::string output;
+  output.reserve(fileSize);
+
+  // read the data:
+  output.insert(output.begin(),
+		std::istream_iterator<char>(file),
+		std::istream_iterator<char>());
+
+  return output;
+}
 
 int main(int argc, char *argv[])
 {
@@ -12,12 +41,13 @@ int main(int argc, char *argv[])
   std::string oldValue;
   
   int c;
-  // r  -- row
-  // c  -- col
-  // m  -- mode
-  // v  -- value
-  // ov -- old value (only applies to CNP)
-  while ((c = getopt(argc, argv, "r:c:m:v:o:")) != -1) {
+  // r -- row
+  // c -- col
+  // m -- mode
+  // v -- value
+  // p -- value path
+  // o -- old value (only applies to CNP)
+  while ((c = getopt(argc, argv, "r:c:m:v:o:p:")) != -1) {
     switch (c) {
     case 'r': {
       row = optarg;
@@ -30,6 +60,9 @@ int main(int argc, char *argv[])
     } break;
     case 'v': {
       value = optarg;
+    } break;
+    case 'p': {
+      value = readFile(optarg);
     } break;
     case 'o': {
       oldValue = optarg;
